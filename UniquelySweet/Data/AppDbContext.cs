359c9a2bc141838d38ff837d4b24dffcf2f3e896
pmbob <petrbobe@gmail.com>
2025-09-29 19:53:53 +0200

    connect to database

---
+++ UniquelySweet/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniquelySweet.Models;
+
+namespace UniquelySweet.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
